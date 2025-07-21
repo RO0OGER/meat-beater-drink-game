bd4bf875555d8f95660553b3ec8c67b7c4cb8cf0
Roger Widmer <ro6widmer@gmail.com>
2025-07-22 00:24:55 +0200

    feat: Added like everything GG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import player from 'lottie-web';
+
+
 
 import { routes } from './app.routes';
+import {provideLottieOptions} from 'ngx-lottie';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideLottieOptions({
+      player: () => player
+    })
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {StartScreen} from '../game/start-screen/start-screen';
+import {CreateRound} from '../game/create-round-screen/create-round';
+import {AddDrinksComponent} from '../game/add-drinks/add-drinks';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: StartScreen },
+  { path: 'create-round', component: CreateRound },
+  { path: 'add-drinks/:round_code', component: AddDrinksComponent,}
+];
